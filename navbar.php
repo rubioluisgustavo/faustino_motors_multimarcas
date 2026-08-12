5ae4da28ac65964fb89c2f1cee5c981f96c5e13d
Luis Gustavo Pilegi Rubio <91914773+rubioluisgustavo@users.noreply.github.com>
2026-08-12 17:17:32 -0300

    link do cabeçalho

--- navbar.php
+++ navbar.php
@@ -28,7 +28,7 @@
         <div class="row align-items-center w-100">
 
             <div class="col-lg-6">
-                <a class="navbar-brand d-flex align-items-center m-0" href="#">
+                <a class="navbar-brand d-flex align-items-center m-0" href="/new/">
                     <img src="img/logo.png" alt="Logo" class="me-3">
 
                     <div>
